2762a53b4d3240a8071107b6cf29cf3e6acf4225
saradomincroft <saradomincroft@gmail.com>
2025-07-02 11:17:01 +1000

    installed frontent react and typescript and axios for api calls

--- server/MiniCMMS/obj/Debug/net8.0/MiniCMMS.AssemblyInfo.cs
+++ server/MiniCMMS/obj/Debug/net8.0/MiniCMMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniCMMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dff98aff07c68d124d24aeca7472aaea9129e6ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniCMMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniCMMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
